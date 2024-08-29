7972d82a157dc6c02296f5d77f477a3bf00cd0a3
Nasr <larko236@outlook.com>
2024-08-29 18:46:51 -0400

    fix: mock

--- src/hooks/useAccount.tsx
+++ src/hooks/useAccount.tsx
@@ -25,28 +25,28 @@ interface AccountContextType {
   clearSession: () => void;
 }
 
-if (!window?.['Telegram']) {
-  mockTelegramEnv({
-    themeParams: {
-      accentTextColor: '#6ab2f2',
-      bgColor: '#17212b',
-      buttonColor: '#5288c1',
-      buttonTextColor: '#ffffff',
-      destructiveTextColor: '#ec3942',
-      headerBgColor: '#17212b',
-      hintColor: '#708499',
-      linkColor: '#6ab3f3',
-      secondaryBgColor: '#232e3c',
-      sectionBgColor: '#17212b',
-      sectionHeaderTextColor: '#6ab3f3',
-      subtitleTextColor: '#708499',
-      textColor: '#f5f5f5',
-    },
-    version: '7.2',
-    platform: 'tdesktop',
-  });
+// if (!window?.['Telegram']) {
+//   mockTelegramEnv({
+//     themeParams: {
+//       accentTextColor: '#6ab2f2',
+//       bgColor: '#17212b',
+//       buttonColor: '#5288c1',
+//       buttonTextColor: '#ffffff',
+//       destructiveTextColor: '#ec3942',
+//       headerBgColor: '#17212b',
+//       hintColor: '#708499',
+//       linkColor: '#6ab3f3',
+//       secondaryBgColor: '#232e3c',
+//       sectionBgColor: '#17212b',
+//       sectionHeaderTextColor: '#6ab3f3',
+//       subtitleTextColor: '#708499',
+//       textColor: '#f5f5f5',
+//     },
+//     version: '7.2',
+//     platform: 'tdesktop',
+//   });
   
-}
+// }
 
 const AccountContext = createContext<AccountContextType | undefined>(undefined);
 
