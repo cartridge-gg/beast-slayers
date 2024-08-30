005349383cbeb725dd99baa1552660d41049f54f
Nasr <larko236@outlook.com>
2024-08-29 23:44:06 -0400

    fix font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      display: ["SuperBoys", "sans-serif"],
+      body: ["SuperBoys", "sans-serif"],
+    },
     container: {
       center: true,
       padding: "2rem",
