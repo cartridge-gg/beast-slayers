dfc96938639fb173415dea5163ce37665159e611
Nasr <larko236@outlook.com>
2024-09-03 23:06:56 -0400

    debug

--- src/hooks/useThingBalance.tsx
+++ src/hooks/useThingBalance.tsx
@@ -7,6 +7,7 @@ export function useThingBalance(client?: ToriiClient, address?: string) {
 
   const updateBalance = (balanceData: any) => {
     setBalance(() => {
+      console.log(balanceData);
       return BigInt(balanceData.balance.value);
     });
   };
--- src/hooks/useWarrior.tsx
+++ src/hooks/useWarrior.tsx
@@ -21,6 +21,7 @@ export function useWarrior(client?: ToriiClient, address?: string) {
 
   const updateWarrior = (warriorData: any) => {
     setWarrior((oldWarrior) => {
+      console.log(warriorData);
       const mappedWarrior = {
         address: warriorData.address.value,
         level: warriorData.level.value,
