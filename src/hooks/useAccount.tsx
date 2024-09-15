2e48a4b88e60fa49f404fdebb953c774e70ab6e8
Nasr <larko236@outlook.com>
2024-09-15 14:22:03 -0400

    revert

--- src/hooks/useAccount.tsx
+++ src/hooks/useAccount.tsx
@@ -120,7 +120,7 @@ export const AccountProvider: React.FC<{ children: React.ReactNode }> = ({ child
     }
 
     const paymaster = JSON.stringify({
-      caller: Dojo.cairoShortStringToFelt("ANY_CALLER")
+      caller: "ANY_CALLER"
     });
     utils.openLink(
       encodeUrl(
